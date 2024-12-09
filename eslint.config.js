54b1ed83946edb03da6e5e2f360a59b9fee1e0fb
partha <spartha343@gmail.com>
2024-12-10 02:36:22 +0600

    project initialized

---
+++ eslint.config.js
@@ -0,0 +1,37 @@
+
+
+/** @type {ESLint.FlatConfig[]} */
+const config = [
+  {
+    files: ["**/*.ts", "**/*.js"],
+    languageOptions: {
+      parser: require("@typescript-eslint/parser"),
+      parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: "module",
+      },
+      globals: {
+        // Replace `env` with appropriate global variable definitions
+        window: "readonly",
+        document: "readonly",
+        navigator: "readonly",
+        process: "readonly",
+        require: "readonly",
+        module: "readonly",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": require("@typescript-eslint/eslint-plugin"),
+    },
+    rules: {
+      "no-console": "error",
+      "no-undef": "error",
+      "no-unused-vars": "error",
+      "no-unused-expressions": "error",
+      "prefer-const": "error",
+      "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+    },
+  },
+];
+
+module.exports = config;
