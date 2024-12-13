b2796983da34a25eb6c0c36e8ba1f428276fe823
partha <spartha343@gmail.com>
2024-12-14 02:03:21 +0600

    vercel fix -7

--- src/shared/prisma.ts
+++ src/shared/prisma.ts
@@ -1,7 +1,21 @@
+import { Pool } from '@neondatabase/serverless';
+import { PrismaNeon } from '@prisma/adapter-neon';
 import { PrismaClient } from '@prisma/client';
 
+// Create a connection pool for NeonDB
+const neon = new Pool({ connectionString: process.env.DATABASE_URL });
+
+// Instantiate PrismaNeon adapter
+const adapter = new PrismaNeon(neon);
+
+// Create Prisma client instance with a custom PrismaNeon adapter (this will need to be manually wired)
 const prisma = new PrismaClient({
-  errorFormat: 'minimal'
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL // Use the URL for the connection
+    }
+  }
 });
 
+// Use `prisma` for operations, this doesn't require the `adapter` to be passed directly
 export default prisma;
